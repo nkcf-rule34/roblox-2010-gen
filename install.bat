3ae6a90f23f33e43e2dc076e5cfd07c7e2de473e
adv <110622030+nkcf-rule34@users.noreply.github.com>
2023-02-01 18:56:46 +0100

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,13 @@
+@echo off
+title Installing Roblox Gen
+pip install requests
+pip install json
+pip install random 
+pip install threading
+pip install sys
+pip install os
+pip install colorama
+color a
+echo Installed!
+pause
+python main.py
